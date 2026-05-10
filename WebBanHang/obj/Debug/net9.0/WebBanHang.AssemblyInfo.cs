3de96f30297a55cc88e45714e9b508c9fb5ca1f9
taka897 <66402318+HuynhTanHuy@users.noreply.github.com>
2026-05-10 23:02:21 +0700

    update

--- WebBanHang/obj/Debug/net9.0/WebBanHang.AssemblyInfo.cs
+++ WebBanHang/obj/Debug/net9.0/WebBanHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+902beb3991474d3692de4f07ec13531ee6b12434")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef44782234c189966005368c5b615705d71591df")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
